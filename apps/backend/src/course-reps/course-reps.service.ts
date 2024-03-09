26eefead2cb7681d4f7734a4b640f2201a6a919a
devodii <emmanuelodii80@gmail.com>
2024-03-09 07:18:28 +0100

    perf: move credentials to env

--- apps/backend/src/course-reps/course-reps.service.ts
+++ apps/backend/src/course-reps/course-reps.service.ts
@@ -1,12 +1,19 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CourseRepAuthDto } from './dto/auth.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class CourseRepsService {
-  public courseReps = [{ email: 'emmanuelodii80@gmail.com', password: '1234' }];
+  constructor(private configService: ConfigService) {}
+
+  private courseRepPassword = this.configService.get('COURSE_REP_PASSWORD');
+  private courseRepeEmail = this.configService.get('COURSE_REP_EMAIL');
+
+  public courseReps = [
+    { email: this.courseRepeEmail, password: this.courseRepPassword },
+  ];
 
   // todo: connect to entity.
-
   async signIn(dto: CourseRepAuthDto) {
     const user = this.courseReps.find(({ email }) => email === dto?.email);
     if (!user || dto?.password !== user.password) {
